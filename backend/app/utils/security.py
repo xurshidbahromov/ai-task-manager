c47bdebd4b2f97931e06d98280ef58d94e05de30
Xurshid <xurshidbahromov06@gmail.com>
2026-01-21 18:38:28 +0500

    chore: Relax CORS policy to allow all origins and provide default values for security configurations.

--- backend/app/utils/security.py
+++ backend/app/utils/security.py
@@ -7,8 +7,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM")
+SECRET_KEY = os.getenv("SECRET_KEY", "fallback_secret_key_for_demo_only_12345")
+ALGORITHM = os.getenv("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
 
 def hash_password(password: str):
